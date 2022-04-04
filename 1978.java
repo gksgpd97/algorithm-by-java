9a76acb05c9047c785e2c543596b98fb8c8d506f
gksgpd97 <gksgpd97@naver.com>
2022-04-04 23:00:24 +0900

    solve boj 1978

---
+++ 1978.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int answer = 0;
+        for (int i = 0; i < N; i++){
+            if (isPrime(arr[i]) == 1)
+                answer++;
+        }
+        bw.write(Integer.toString(answer));
+        bw.flush();
+        bw.close();
+        br.close();
+        
+	}
+
+    public static int isPrime(int n){
+        if (n == 1)
+            return 0;
+        else if (n == 2)
+            return 1;
+        else{
+            for (int i = 2; i < n; i++){
+                if (n % i == 0)
+                    return 0;
+            }
+        }
+        return 1;
+    }
+}
