39e78de2cf405809f77426a9692440b859d982f4
gksgpd97 <gksgpd97@naver.com>
2022-04-17 01:20:59 +0900

    solve boj 15651

--- 15651.java
+++ 15651.java
@@ -2,7 +2,6 @@ import java.util.*;
 import java.io.*;
 public class Main {
     static int[] arr = new int[8];
-    static int[] visited = new int[8];
     static int N;
     static int M;
     static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -21,31 +20,17 @@ public class Main {
         br.close();
         
 	}
-    // N = 4 M = 2
     public static void bt(int k)throws IOException{
         if (k == M){
             for (int i=0; i<M; i++){
-                bw.write(Integer.toString(arr[i]));
+                bw.write(Integer.toString(arr[i])+" ");
             }
             bw.write("\n");
             return;
         }
         for (int i=1; i<=N; i++){
-            if (k==2 && visited[i] == 0){
                 arr[k] = i;
-                visited[i] = 1;
                 bt(k+1);
-                visited[i] = 0;    
-            }
         }
     }
 }
-/*
-visited
-0 1 2 3 4 5 6 7 8
-0 1 1
-
-arr
-0 1 2 3 4 5 6 7 8
-1 2
-*/
